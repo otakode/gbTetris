01a3453d84f99cd7be372cd43b07737eeacb2d40
Clement Marchal <cmarchal@workgroup.aso>
2019-09-18 14:01:27 +0200

    add game and tools dedicated asm files

--- main.asm
+++ main.asm
@@ -52,6 +52,7 @@ Start:
 Main:
 	; Refresh Inputs
 	; Game processing
+	call Game
 .waitVBlank
 	; Set the VBlank Interrupt
 	ld a, IEF_VBLANK
@@ -68,37 +69,9 @@ Main:
 	ld [rIE], a
 	jr Main
 
+INCLUDE "game.asm"
 
-	; --- Memcpy ---
-	; @param hl ; address to copy to ; return address after data copy
-	; @param de ; address to copy from ; return address after data copy
-	; @param bc ; byte size of data to copy ; return 0
-	; @flags ; a = 0 ; C const ; H unk ; N- ; Z set
-Memcpy:
-	ld a, [de]
-	ld [hli], a ; hli increments hl after use
-	inc de
-	dec bc
-	ld a, b
-	or c
-	jr nz, Memcpy
-	ret
-	; --- End Memcpy ---
-
-
-	; --- StrCpy ---
-	; @param hl ; address to copy to ; return address after data copy
-	; @param de ; address to copy from ; return address after data copy
-	; @flags ; a = 0 ; C const ; H unk ; N+ ; Z set
-StrCpy:
-	ld a, [de]
-	ld [hli], a
-	inc de
-	and a ; test if zero
-	jr nz, StrCpy
-	ret
-	; --- End StrCpy ---
-
+INCLUDE "tools.asm"
 
 SECTION "FONT", ROM0
 
---
+++ tools.asm
@@ -0,0 +1,31 @@
+	; Tools Functions
+
+	; --- Memcpy ---
+	; @param hl ; address to copy to ; return address after data copy
+	; @param de ; address to copy from ; return address after data copy
+	; @param bc ; byte size of data to copy ; return 0
+	; @flags ; a = 0 ; C const ; H unk ; N- ; Z set
+Memcpy:
+	ld a, [de]
+	ld [hli], a ; hli increments hl after use
+	inc de
+	dec bc
+	ld a, b
+	or c
+	jr nz, Memcpy
+	ret
+	; --- End Memcpy ---
+
+
+	; --- StrCpy ---
+	; @param hl ; address to copy to ; return address after data copy
+	; @param de ; address to copy from ; return address after data copy
+	; @flags ; a = 0 ; C const ; H unk ; N+ ; Z set
+StrCpy:
+	ld a, [de]
+	ld [hli], a
+	inc de
+	and a ; test if zero
+	jr nz, StrCpy
+	ret
+	; --- End StrCpy ---
