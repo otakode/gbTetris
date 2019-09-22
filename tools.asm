3896ffdd716fc877c0eb4ebfe95e29a0c2fe8b27
Otakode <marchal.cl@gmail.com>
2019-09-22 08:01:01 +0200

    optimize hexToStr functions + flags state
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- tools.asm
+++ tools.asm
@@ -5,7 +5,7 @@ SECTION "Tools Functions", ROM0
 	; @param hl ; address to copy to ; return address after data copy
 	; @param de ; address to copy from ; return address after data copy
 	; @param bc ; byte size of data to copy ; return 0
-	; @flags ; a = 0 ; C const ; H unk ; N- ; Z set
+	; @flags ; a = 0 ; C unchanged ; H unchanged ; N = true ; Z = true
 Memcpy:
 	ld a, [de]
 	ld [hli], a ; hli increments hl after use
@@ -21,7 +21,7 @@ Memcpy:
 	; --- StrCpy ---
 	; @param hl ; address to copy to ; return address after data copy
 	; @param de ; address to copy from ; return address after data copy
-	; @flags ; a = 0 ; C const ; H unk ; N+ ; Z set
+	; @flags ; a = 0 ; C unchanged ; H unchanged ; N = true ; Z = true
 StrCpy:
 	ld a, [de]
 	ld [hli], a
@@ -35,11 +35,14 @@ StrCpy:
 	; --- R8ToStr ---
 	; @param hl ; address to write string to (3 chars + null) ; return address to the character at the start of the string
 	; @param d ; value of the number ; return unchanged
-	; @param bc ; out param ; return size of the string
 	; @flags ; a? ; C? ; H? ; N? ; Z?
 R8ToStr:
 	; just write in hexadecimal for now...
+	ld [hl], "0"
+	inc hl
 	call R8ToHexStr
+	ld [hl], 0
+	inc hl
 
 	ret
 	; --- End R8ToStr ---
@@ -48,119 +51,89 @@ R8ToStr:
 	; --- R16ToStr ---
 	; @param hl ; address to write string to (5 chars + null) ; return address to the character at the start of the string
 	; @param de ; value of the number ; return unchanged
-	; @param bc ; out param ; return size of the string
 	; @flags ; a? ; C? ; H? ; N? ; Z?
 R16ToStr:
 	; just write in hexadecimal for now...
+	ld [hl], "0"
+	inc hl
 	call R16ToHexStr
+	ld [hl], 0
+	inc hl
 
 	ret
 	; --- End R16ToStr ---
 
 
 	; --- R8ToHexStr ---
-	; @param hl ; address to write string to (3 chars + null) ; return address to the character at the start of the string
+	; @param hl ; address to write string to (2 chars) ; return address to the character at the start of the string
 	; @param d ; value of the number ; return unchanged
-	; @param bc ; out param ; return size of the string
-	; @flags ; a? ; C? ; H? ; N? ; Z?
+	; @flags ; a = last character ; C = is last character a letter ; H = C ; N = true ; Z = false
 R8ToHexStr:
-	ld c, 3
-	ld b, 0
-	add hl, bc
-
-	ld [hl], 0 ; null terminator
-	dec hl
-
-	ld a, d
-	and $0F
-	cp $0A
-	jr c, .notHex3
-	add "A" - 10
-	jr .write3
-.notHex3
-	add "0"
-.write3
-	ld [hld], a
-
 	ld a, d
 	swap a
 	and $0F
 	cp $0A
+	jr c, .notHex1
+	add "A" - 10 - "0"
+.notHex1
+	add "0"
+	ldi [hl], a
+
+	ld a, d
+	and $0F
+	cp $0A
 	jr c, .notHex2
-	add "A" - 10
-	jr .write2
+	add "A" - 10 - "0"
 .notHex2
 	add "0"
-.write2
-	ld [hld], a
-
-	ld [hl], "0"
+	ldi [hl], a
 
 	ret
 	; --- End R8ToHexStr ---
 
 
 	; --- R16ToHexStr ---
-	; @param hl ; address to write string to (5 chars + null) ; return address to the character at the start of the string
+	; @param hl ; address to write string to (4 chars) ; return address after the string (+4)
 	; @param de ; value of the number ; return unchanged
-	; @param bc ; out param ; return size of the string
-	; @flags ; a? ; C? ; H? ; N? ; Z?
+	; @flags ; a = last character ; C = is last character a letter ; H = C ; N = true ; Z = false
 R16ToHexStr:
-	; just write in hexadecimal for now...
-	ld c, 5
-	ld b, 0
-	add hl, bc
-
-	ld [hl], 0 ; null terminator
-	dec hl
-
-	ld a, d
-	and $0F
-	cp $0A
-	jr c, .notHex5
-	add "A" - 10
-	jr .write5
-.notHex5
-	add "0"
-.write5
-	ld [hld], a
-
-	ld a, d
-	swap a
-	and $0F
-	cp $0A
-	jr c, .notHex4
-	add "A" - 10
-	jr .write4
-.notHex4
-	add "0"
-.write4
-	ld [hld], a
-
-	ld a, e
-	and $0F
-	cp $0A
-	jr c, .notHex3
-	add "A" - 10
-	jr .write3
-.notHex3
-	add "0"
-.write3
-	ld [hld], a
-
 	ld a, e
 	swap a
+	and $0F
+	cp $0A
+	jr c, .notHex1
+	add "A" - 10 - "0"
+.notHex1
+	add "0"
+	ldi [hl], a
+
+	ld a, e
 	and $0F
 	cp $0A
 	jr c, .notHex2
-	add "A" - 10
-	jr .write2
+	add "A" - 10 - "0"
 .notHex2
 	add "0"
-.write2
-	ld [hld], a
+	ldi [hl], a
 
-	ld [hl], "0"
+	ld a, d
+	swap a
+	and $0F
+	cp $0A
+	jr c, .notHex3
+	add "A" - 10 - "0"
+.notHex3
+	add "0"
+	ldi [hl], a
+
+	ld a, d
+	and $0F
+	cp $0A
+	jr c, .notHex4
+	add "A" - 10 - "0"
+.notHex4
+	add "0"
+	ldi [hl], a
 
 	ret
 	; --- End R16ToHexStr ---
