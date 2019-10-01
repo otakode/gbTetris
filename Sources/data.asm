71b5cf6b374b9b9bb086ba161b452fb0a252ee38
Otakode <marchal.cl@gmail.com>
2019-10-02 00:03:18 +0200

    Options TileMap
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- Sources/data.asm
+++ Sources/data.asm
@@ -4,9 +4,6 @@
 INCLUDE "../TileSet/OtakodeLogoTileSet.z80"
 OtakodeLogoTileSetEnd:
 
-INCLUDE "../TileSet/GameBoyTileSet.z80"
-GameBoyTileSetEnd:
-
 INCLUDE "../TileSet/FontTileSet.z80"
 FontTileSetEnd:
 
@@ -18,10 +15,10 @@ FontTileSetEnd:
 INCLUDE "../TileMap/OtakodeLogoTileMap.z80"
 OtakodeLogoTileMapEnd:
 
-INCLUDE "../TileMap/GameBoyTileMap.z80"
-GameBoyTileMapEnd:
-
 INCLUDE "../TileMap/TitleTileMap.z80"
 TitleTileMapEnd:
+
+INCLUDE "../TileMap/OptionsTileMap.z80"
+OptionsTileMapEnd:
 
 ; --- End TileMaps ---
